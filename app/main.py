ae49fce16205dd8297c26c92f80e4672a96f2863
Paweł Dudek <dudek136@gmail.com>
2025-10-15 23:07:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,51 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class, clean_mark, brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_fron_city_center, clean_power, average_rating,
+                 count_of_ratings):
+        self.distance_from_city_center = distance_fron_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
+
+    def calculate_washing_price(self, car):
+        income = 0.0
+        income = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center
+                )
+        income = round(income, 1)
+        return income
+
+    def wash_single_car(self, car):
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, number):
+        self.average_rating = (
+                                (
+                                self.average_rating
+                                * self.count_of_ratings
+                                + number
+                                 )
+                                / (
+                                self.count_of_ratings + 1
+                                )
+                                )
+        self.average_rating = round(self.average_rating, 1)
+        self.count_of_ratings += 1
+
